f67fc3de7addfdcf85deffa421871a0e51366834
Karl255 <49595438+Karl255@users.noreply.github.com>
2025-06-01 16:33:55 +0200

    PinList prep

---
+++ web/src/lib/components/PinList.svelte
@@ -0,0 +1,9 @@
+<script lang="ts"></script>
+
+<div class="container"></div>
+
+<style>
+	.container {
+		background-color: var(--color-zet);
+	}
+</style>
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,12 +1,30 @@
 <script lang="ts">
+	import PinList from '$lib/components/PinList.svelte';
 	import { MapLibre } from 'svelte-maplibre';
 </script>
 
-<MapLibre
-	style="/map-styles/osm-liberty.json"
-	standardControls="bottom-left"
-	center={[15.985, 45.8]}
-	zoom={12}
-	dragRotate={false}
-	pitchWithRotate={false}
-/>
+<div class="map-container">
+	<MapLibre
+		class="maplibre"
+		style="/map-styles/osm-liberty.json"
+		standardControls="bottom-left"
+		center={[15.985, 45.8]}
+		zoom={12}
+		dragRotate={false}
+		pitchWithRotate={false}
+	/>
+
+	<PinList />
+</div>
+
+<style>
+	.map-container {
+		height: 100%;
+		display: grid;
+		grid-template-columns: 1fr min(480px, 40%);
+	}
+
+	:global(.maplibre) {
+		--_: 'empty';
+	}
+</style>
