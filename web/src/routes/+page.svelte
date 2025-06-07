4c51a609fc0cff1aebfa514aeef18518c780c32b
Karl255 <49595438+Karl255@users.noreply.github.com>
2025-06-07 13:10:48 +0200

    location select

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
 	import PinList from '$lib/components/PinList.svelte';
+	import { LngLat, MapMouseEvent, type LngLatLike } from 'maplibre-gl';
 	import { DefaultMarker, MapLibre } from 'svelte-maplibre';
 
-	type Pin = [number, number];
+	const pins: LngLatLike[] = [[15.985, 45.8]];
 
-	const pins: Pin[] = [[15.985, 45.8]];
+	let selectedLocation: LngLat | null = $state(null);
+
+	function selectLocation(e: MapMouseEvent) {
+		selectedLocation = e.lngLat;
+	}
 </script>
 
-<div class="map-container">
+<div class="container">
 	<MapLibre
 		class="maplibre"
 		style="/map-styles/osm-liberty.json"
@@ -16,17 +21,21 @@
 		zoom={12}
 		dragRotate={false}
 		pitchWithRotate={false}
+		onclick={selectLocation}
 	>
 		{#each pins as pin (pin)}
 			<DefaultMarker lngLat={pin} />
 		{/each}
+		{#if selectedLocation !== null}
+			<DefaultMarker lngLat={selectedLocation} />
+		{/if}
 	</MapLibre>
 
 	<PinList />
 </div>
 
 <style>
-	.map-container {
+	.container {
 		height: 100%;
 		display: grid;
 		grid-template-columns: 1fr min(480px, 40%);
