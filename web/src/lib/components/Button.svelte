4d438f0e1451052fd6a088ff98a2121949fb8699
Karl255 <49595438+Karl255@users.noreply.github.com>
2025-06-07 15:43:19 +0200

    full button stling

--- web/src/lib/components/Button.svelte
+++ web/src/lib/components/Button.svelte
@@ -18,18 +18,29 @@
 <style>
 	.button {
 		padding: 10px 20px;
+		background-color: var(--_bgc);
+		box-shadow: var(--shadow);
 		border: none;
 		cursor: pointer;
+
+		transition:
+			background-color 150ms ease-out,
+			box-shadow 150ms ease-out;
+
+		&:hover {
+			background-color: lch(from var(--_bgc) calc(l - 10) c h);
+			box-shadow: var(--shadow-hover);
+		}
 
 		:global {
 			&.primary {
 				color: white;
-				background-color: var(--color-primary);
+				--_bgc: var(--color-primary);
 			}
 
 			&.primary-dark {
 				color: white;
-				background-color: var(--color-primary-dark);
+				--_bgc: var(--color-primary-dark);
 			}
 
 			&.large {
