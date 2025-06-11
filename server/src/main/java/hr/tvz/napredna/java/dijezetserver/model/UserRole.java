2c9fc69f6662834ab860d97e242b0b15f8841b25
recycleTheC <mario.kopjar1@gmail.com>
2025-06-11 23:29:37 +0200

    Changed user role

--- server/src/main/java/hr/tvz/napredna/java/dijezetserver/model/UserRole.java
+++ server/src/main/java/hr/tvz/napredna/java/dijezetserver/model/UserRole.java
@@ -3,7 +3,7 @@ package hr.tvz.napredna.java.dijezetserver.model;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum UserRole implements GrantedAuthority {
-    ADMIN,
+    OPERATOR,
     USER;
 
     @Override
