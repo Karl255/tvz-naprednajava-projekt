b35b9fa9c99904705deb7e535a0b34ffd97db312
Karl255 <49595438+Karl255@users.noreply.github.com>
2025-06-11 15:18:38 +0200

    add pin api

---
+++ web/src/lib/constants/api.ts
@@ -0,0 +1,8 @@
+const BASE_PATH = '/di-je-zet/api';
+
+export const API_PATH = {
+	PIN: BASE_PATH + 'pin',
+	COMMENT: BASE_PATH + 'comment',
+	LINE: BASE_PATH + 'line',
+	STATION: BASE_PATH + 'station',
+};
