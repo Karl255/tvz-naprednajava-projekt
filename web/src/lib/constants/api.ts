f397e7dcb6bbf5c894aaee6c03883f877b0ca063
Karl255 <49595438+Karl255@users.noreply.github.com>
2025-06-11 17:05:22 +0200

    connect BE and FE

--- web/src/lib/constants/api.ts
+++ web/src/lib/constants/api.ts
@@ -1,8 +1,11 @@
-const BASE_PATH = '/di-je-zet/api';
+const url = new URL(window.location.href);
+url.port = '8080';
+const BASE_PATH = url.origin + '/di-je-zet/api';
+console.log('BASE_PATH', BASE_PATH);
 
 export const API_PATH = {
-	PIN: BASE_PATH + 'pin',
-	COMMENT: BASE_PATH + 'comment',
-	LINE: BASE_PATH + 'line',
-	STATION: BASE_PATH + 'station',
+	PIN: BASE_PATH + '/pins',
+	COMMENT: BASE_PATH + '/comments',
+	LINE: BASE_PATH + '/lines',
+	STATION: BASE_PATH + '/stations',
 };
