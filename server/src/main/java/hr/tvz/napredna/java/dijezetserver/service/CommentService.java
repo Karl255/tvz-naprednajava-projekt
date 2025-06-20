3b1828aa57444863f8f28c5bdbdc6d1b3c342642
recycleTheC <mario.kopjar1@gmail.com>
2025-06-20 21:31:48 +0200

    Passing logged in user from request on creating pins and comments (#44)

--- server/src/main/java/hr/tvz/napredna/java/dijezetserver/service/CommentService.java
+++ server/src/main/java/hr/tvz/napredna/java/dijezetserver/service/CommentService.java
@@ -1,6 +1,7 @@
 package hr.tvz.napredna.java.dijezetserver.service;
 
 import hr.tvz.napredna.java.dijezetserver.dto.CommentDto;
+import hr.tvz.napredna.java.dijezetserver.model.User;
 import hr.tvz.napredna.java.dijezetserver.request.CommentRequest;
 
 import java.util.List;
@@ -8,7 +9,7 @@ import java.util.List;
 public interface CommentService {
     List<CommentDto> findAll();
 
-    CommentDto create(CommentRequest commentRequest);
+    CommentDto create(CommentRequest commentRequest, User user);
 
     CommentDto update(Long id, CommentRequest commentRequest);
 
