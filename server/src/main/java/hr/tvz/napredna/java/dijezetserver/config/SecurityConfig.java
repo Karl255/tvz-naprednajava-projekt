0c593a773987138353f9624f4b563ce499fe3c79
Karl255 <49595438+Karl255@users.noreply.github.com>
2025-07-02 20:48:01 +0200

    S

--- server/src/main/java/hr/tvz/napredna/java/dijezetserver/config/SecurityConfig.java
+++ server/src/main/java/hr/tvz/napredna/java/dijezetserver/config/SecurityConfig.java
@@ -16,24 +16,21 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
 
     private final UserDetailsService userDetailsService;
     private final String jwtSecret;
+    private final CorsConfig corsConfig;
 
-    public SecurityConfig(UserDetailsService userDetailsService, @Value("${jwt.secret}") String jwtSecret) {
+    public SecurityConfig(UserDetailsService userDetailsService, @Value("${jwt.secret}") String jwtSecret, CorsConfig corsConfig) {
         this.userDetailsService = userDetailsService;
         this.jwtSecret = jwtSecret;
+        this.corsConfig = corsConfig;
     }
 
     @Bean
@@ -55,7 +52,7 @@ public class SecurityConfig {
                                 .anyRequest().authenticated()
                 )
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()));
+                .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()));
 
         http.addFilterBefore(jwtTokenFromCookieFilter, UsernamePasswordAuthenticationFilter.class);
         http.sessionManagement(config -> config.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
@@ -74,18 +71,5 @@ public class SecurityConfig {
         authenticationProvider.setPasswordEncoder(passwordEncoder);
 
         return new ProviderManager(authenticationProvider);
-    }
-
-    @Bean
-    UrlBasedCorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(CorsConfiguration.ALL));
-        configuration.setAllowedMethods(List.of(CorsConfiguration.ALL));
-        configuration.setAllowedHeaders(List.of(CorsConfiguration.ALL));
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration(ApiPaths.BASE_API_URL + "/**", configuration);
-
-        return source;
     }
 }
